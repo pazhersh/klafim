04fee8d7015a396a4c16b4232a287498d582d895
pazhersh <pazhersh@gmail.com>
2025-05-31 18:08:04 +0300

    format code

--- src/main.js
+++ src/main.js
@@ -2,17 +2,17 @@ import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
 const loader = new GLTFLoader();
 const deckGLTF = await loader.loadAsync('/assets/deck.glb');
 
 const deck = deckGLTF.scene;
-scene.add( deck );
+scene.add(deck);
 
 const skyColor = 0xFFFFFF;
 const groundColor = 0x101010;
@@ -26,6 +26,6 @@ function animate() {
   deck.rotation.x += 0.01
   deck.rotation.y += 0.01
 
-  renderer.render( scene, camera );
+  renderer.render(scene, camera);
 }
-renderer.setAnimationLoop( animate );
+renderer.setAnimationLoop(animate);
