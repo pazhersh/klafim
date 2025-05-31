2449ceb3fd1e1d0c7fdbba42970dcda9e21d0904
pazhersh <pazhersh@gmail.com>
2025-05-31 18:07:17 +0300

    added a mock deck model

--- src/main.js
+++ src/main.js
@@ -1,25 +1,31 @@
 import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
-renderer.setAnimationLoop( animate );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+const loader = new GLTFLoader();
+const deckGLTF = await loader.loadAsync('/assets/deck.glb');
+
+const deck = deckGLTF.scene;
+scene.add( deck );
+
+const skyColor = 0xFFFFFF;
+const groundColor = 0x101010;
+const light = new THREE.HemisphereLight(skyColor, groundColor, 1);
+scene.add(light);
+
 
 camera.position.z = 5;
 
 function animate() {
-
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+  deck.rotation.x += 0.01
+  deck.rotation.y += 0.01
 
   renderer.render( scene, camera );
-
 }
+renderer.setAnimationLoop( animate );
