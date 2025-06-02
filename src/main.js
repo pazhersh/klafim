bbb6021abf1231527cfbd748139ac2cd3b6ce2ad
pazhersh <pazhersh@gmail.com>
2025-06-02 16:50:56 +0300

    don't move when clicking on deck

--- src/main.js
+++ src/main.js
@@ -5,13 +5,15 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { Raycaster } from './raycaster.js';
 
 const scene = new THREE.Scene();
+
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 5;
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 
 const canvasElement = renderer.domElement;
-document.body.appendChild(canvasElement);
+const raycaster = new Raycaster(camera, sceneObjects, canvasElement)
 
 const loader = new GLTFLoader();
 const deckGLTF = await loader.loadAsync('/assets/deck.glb');
@@ -27,8 +29,17 @@ const groundColor = 0x101010;
 const light = new THREE.HemisphereLight(skyColor, groundColor, 1);
 scene.add(light);
 
+document.body.appendChild(canvasElement);
+
 const orbitControls = new OrbitControls(camera, canvasElement);
-camera.position.z = 5;
+let isOrbiting = false;
+orbitControls.addEventListener('start', () => { isOrbiting = true });
+orbitControls.addEventListener('end', () => { isOrbiting = false });
+canvasElement.addEventListener('mousemove', (event) => {
+  const selectedElement = raycaster.getPointedElement(event);
+  if (!isOrbiting)
+    orbitControls.enabled = !Boolean(selectedElement);
+})
 
 const gridHelper = new THREE.GridHelper(10, 10);
 scene.add(gridHelper);
@@ -38,7 +49,6 @@ function animate() {
 }
 renderer.setAnimationLoop(animate);
 
-const raycaster = new Raycaster(camera, sceneObjects, canvasElement)
 
 canvasElement.addEventListener('mousedown', (event) => {
   const selectedElement = raycaster.getPointedElement(event);
