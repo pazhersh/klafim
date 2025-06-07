7ccf60a4742d976d87625dbe98e7e4c047764659
pazhersh <pazhersh@gmail.com>
2025-06-07 23:38:57 +0300

    added rudimentary DI

--- src/card.js
+++ src/card.js
@@ -2,18 +2,19 @@ import * as THREE from 'three';
 import RAPIER from '@dimforge/rapier3d-compat/rapier.es.js';
 
 export class Card {
-    constructor(scene, world) {
+    constructor() {
         const meshGeometry = new THREE.BoxGeometry(1, 1, 1);
         const meshMaterial = new THREE.MeshBasicMaterial({ color: 0xff00ff });
         this.mesh = new THREE.Mesh(meshGeometry, meshMaterial);
-        scene.add(this.mesh);
+        document.scene.add(this.mesh);
 
         const rigidBodyDesc = RAPIER.RigidBodyDesc.dynamic()
-            .setTranslation(0.0, 1.0, 0.0);
+            .setTranslation(0.0, 2.0, 0.0)
+            .setRotation({ w: 1.0, x: 0.5, y: 0.5, z: 0.0 });
 
-        this.rigidBody = world.createRigidBody(rigidBodyDesc);
+        this.rigidBody = document.world.createRigidBody(rigidBodyDesc);
         const colliderDesc = RAPIER.ColliderDesc.cuboid(1.0, 1.0, 1.0);
-        world.createCollider(colliderDesc, this.rigidBody);
+        document.world.createCollider(colliderDesc, this.rigidBody);
     }
 
 
@@ -21,6 +22,6 @@ export class Card {
         const position = this.rigidBody.translation();
         this.mesh.position.copy(new THREE.Vector3(position.x, position.y, position.z));
         const rotation = this.rigidBody.rotation();
-        this.mesh.rotation.copy(new THREE.Euler(rotation.x, rotation.y, rotation.z));
+        this.mesh.setRotationFromEuler(new THREE.Euler(rotation.x, rotation.y, rotation.z));
     }
 }
--- src/main.js
+++ src/main.js
@@ -13,15 +13,17 @@ await RAPIER.init();
 // Use the RAPIER module here.
 let gravity = { x: 0.0, y: -9.81, z: 0.0 };
 let world = new RAPIER.World(gravity);
+document.world = world;
 
 // Create the ground
-const groundBody = world.createRigidBody(RAPIER.RigidBodyDesc.fixed().setTranslation(0, -1, 0))
+const groundBody = document.world.createRigidBody(RAPIER.RigidBodyDesc.fixed().setTranslation(0, -1, 0))
 const groundCollider = RAPIER.ColliderDesc.cuboid(50, 0.5, 50)
-world.createCollider(groundCollider, groundBody)
+document.world.createCollider(groundCollider, groundBody)
 
 
 
-const scene = new THREE.Scene();
+
+document.scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 5;
@@ -35,19 +37,19 @@ const loader = new GLTFLoader();
 const deckGLTF = await loader.loadAsync('/assets/deck.glb');
 
 const gridHelper = new THREE.GridHelper(100, 100);
-scene.add(gridHelper);
+document.scene.add(gridHelper);
 
 const deck = deckGLTF.scene.children[0]; // not the cleanest but hey, it's just a side-project
 const cards = [];
 
 const sceneObjects = [deck];
 
-scene.add(...sceneObjects);
+document.scene.add(...sceneObjects);
 
 const skyColor = 0xFFFFFF;
 const groundColor = 0x101010;
 const light = new THREE.HemisphereLight(skyColor, groundColor, 1);
-scene.add(light);
+document.scene.add(light);
 
 document.body.appendChild(canvasElement);
 
@@ -71,7 +73,7 @@ function addCard(position) {
   const card = new THREE.Mesh(cardGeometry, cardMaterial);
   // card.position.set(position ?? new THREE.Vector3(0, 0, 0));
 
-  scene.add(card);
+  document.scene.add(card);
   sceneObjects.push(card);
   cards.push(card);
 
@@ -92,7 +94,7 @@ canvasElement.addEventListener('mousedown', (event) => {
 
 
 
-const element = new Card(scene, world);
+const element = new Card();
 
 
 // // Feels like this should be on of the animation function, but it breaks the physics for some reason
@@ -102,10 +104,10 @@ function animate() {
   requestAnimationFrame(animate);
 
   // delta = clock.getDelta();
-  // world.timestep = delta;
-  world.step();
+  // document.world.timestep = delta;
+  document.world.step();
   element.update();
 
-  renderer.render(scene, camera);
+  renderer.render(document.scene, camera);
 }
 renderer.setAnimationLoop(animate);
