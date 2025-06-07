31c3a9a78e91bce9c23339baec8afbf1eea701d4
pazhersh <pazhersh@gmail.com>
2025-06-07 20:18:19 +0300

    added basic phsics elements

--- src/main.js
+++ src/main.js
@@ -5,6 +5,25 @@ import { DragControls } from 'three/addons/controls/DragControls.js';
 
 import { Raycaster } from './raycaster.js';
 
+import RAPIER from '@dimforge/rapier3d-compat/rapier.es.js';
+
+await RAPIER.init();
+
+// Use the RAPIER module here.
+let gravity = { x: 0.0, y: -9.81, z: 0.0 };
+let world = new RAPIER.World(gravity);
+
+// Create the ground
+const groundBody = world.createRigidBody(RAPIER.RigidBodyDesc.fixed().setTranslation(0, -1, 0))
+const groundShape = RAPIER.ColliderDesc.cuboid(50, 0.5, 50)
+world.createCollider(groundShape, groundBody)
+
+
+
+const rapierDebug = world.debugRender();
+
+
+
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -74,7 +93,47 @@ canvasElement.addEventListener('mousedown', (event) => {
   }
 })
 
+
+const debugMesh = new THREE.LineSegments(new THREE.BufferGeometry(), new THREE.LineBasicMaterial({ color: 'lime' }));
+debugMesh.frustrumCulled = false;
+debugMesh.geometry.setAttribute('position', new THREE.BufferAttribute(rapierDebug.vertices, 3));
+debugMesh.visible = true;
+scene.add(debugMesh);
+
+const elementGeometry = new THREE.BoxGeometry(1, 1, 1);
+const elementMaterial = new THREE.MeshBasicMaterial({ color: 0xff00ff });
+
+const elementBodyDesc = RAPIER.RigidBodyDesc.dynamic().setTranslation(0.0, 1.0, 0.0);
+const elementBody = world.createRigidBody(elementBodyDesc);
+const elementBodyShape = RAPIER.ColliderDesc.cuboid(1, 1, 1).setMass(1);
+world.createCollider(elementBodyShape, elementBody);
+
+
+const element = {
+  physicsBody: elementBody,
+  mesh: new THREE.Mesh(elementGeometry, elementMaterial)
+}
+
+scene.add(element.mesh);
+
+
+
+function updateElement() {
+  element.mesh.position.copy(element.physicsBody.translation());
+  element.mesh.rotation.copy(element.physicsBody.rotation());
+}
+
+const clock = new THREE.Clock()
+let delta;
+
 function animate() {
+  requestAnimationFrame(animate);
+
+  delta = clock.getDelta();
+  world.timestep = delta;
+  world.step();
+
   renderer.render(scene, camera);
+  updateElement();
 }
 renderer.setAnimationLoop(animate);
