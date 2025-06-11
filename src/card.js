1adecf062ffad43a4870a3876b8a07cec710fd3b
pazhersh <pazhersh@gmail.com>
2025-06-11 23:07:47 +0300

    added the silliest floating mechanics

--- src/card.js
+++ src/card.js
@@ -1,7 +1,10 @@
 import * as THREE from 'three';
 import RAPIER from '@dimforge/rapier3d-compat/rapier.es.js';
 
-export class Card {
+export default class Card {
+    wasClicked = false;
+    targetMovement = new THREE.Vector3();
+
     constructor() {
         const meshGeometry = new THREE.BoxGeometry(1, 1, 1);
         const meshMaterial = new THREE.MeshBasicMaterial({ color: 0xff00ff });
@@ -9,28 +12,68 @@ export class Card {
         document.scene.add(this.mesh);
 
         const rigidBodyDesc = RAPIER.RigidBodyDesc.dynamic()
-            .setTranslation(0.0, 2.0, 0.0)
-            .setRotation({ w: 1.0, x: 0.5, y: 0.5, z: 0.0 });
+            .setTranslation(0.0, 2.0, 0.0);
 
         this.rigidBody = document.world.createRigidBody(rigidBodyDesc);
-        // const points = this.mesh.geometry.attributes.position.array;
-        // const colliderDesc = RAPIER.ColliderDesc
-        //     .convexHull(points)
-        //     .setDensity(1.0);
-        const colliderDesc = RAPIER.ColliderDesc.cuboid(1.0, 1.0, 1.0);
+        const points = this.mesh.geometry.attributes.position.array;
+        const colliderDesc = RAPIER.ColliderDesc
+            .convexHull(points)
+            .setDensity(1.0);
         document.world.createCollider(colliderDesc, this.rigidBody);
     }
 
+    hover(targetPosition) {
+        const position = this.rigidBody.translation();
+        const movementForce = targetPosition.clone()
+            .sub(position)
+            .divideScalar(100);
+
+        const velocity = this.rigidBody.linvel();
+        const velocityVector = new THREE.Vector3(velocity.x, velocity.y, velocity.z);
+        // const dragForce = 1/2 * density * velocity^2 * Drag coefficient * cross sectional area
+        const dragForce =
+            velocityVector.clone().multiply(velocityVector) // velocity squeared
+                .divideScalar(2)
+                .multiplyScalar(Math.pow(targetPosition.distanceTo(position), -1)) // density
+                .multiplyScalar(1) // Drag coefficient
+                .multiplyScalar(1) // cross sectional area
+                .multiplyScalar(-1) // against the movement direction
+
+        const totalForce = velocityVector.clone().add(movementForce);
+        // console.log(force, dragVector);
+        this.rigidBody.resetForces(true);
+        this.rigidBody.addForce(movementForce, true);
+        this.rigidBody.addForce(dragForce, true);
+        // this.rigidBody.addForce(totalForce, true);
+    }
 
     update() {
-        const position = this.rigidBody.translation();
-        this.mesh.position.copy(new THREE.Vector3(position.x, position.y, position.z));
+        this.mesh.position.copy(this.rigidBody.translation());
         const rotation = this.rigidBody.rotation();
         this.mesh.setRotationFromEuler(new THREE.Euler(rotation.x, rotation.y, rotation.z));
+        if (this.wasClicked)
+            this.hover(this.targetMovement);
     }
 
     onClick(position) {
-        const force = this.mesh.position.clone().sub(position).normalize();
-        this.rigidBody.addForce(new RAPIER.Vector3(force.x, force.y, force.z), true);
+        // const force = this.mesh.position.clone().sub(position).normalize().divideScalar(-2);
+        // this.rigidBody.addForce(new RAPIER.Vector3(force.x, force.y, force.z), true);
+
+        this.rigidBody.setGravityScale(0);
+        this.wasClicked = true;
+
+        const bodyPosition = this.rigidBody.translation();
+        this.targetMovement = new THREE.Vector3(bodyPosition.x, bodyPosition.y + 1, bodyPosition.z + 3)
+
+        // this.rigidBody.applyImpulse(new RAPIER.Vector3(0, 0, 1), true);
+        // setTimeout(() => {
+        //     this.rigidBody.applyImpulse(new RAPIER.Vector3(0, 0, -2), true);
+        // }, 250);
+    }
+    onDrag(position) {
+
+    }
+    onRelease(position) {
+
     }
 }
