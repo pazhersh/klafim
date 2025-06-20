5114df4148af77757c2b8894ff436ba91c4b3dda
pazhersh <pazhersh@gmail.com>
2025-06-20 18:29:21 +0300

    small comment

--- src/card.js
+++ src/card.js
@@ -50,6 +50,7 @@ export default class Card {
     update() {
         this.mesh.position.copy(this.rigidBody.translation());
         const rotation = this.rigidBody.rotation();
+        // This is the wrong calculation
         this.mesh.setRotationFromEuler(new THREE.Euler(rotation.x, rotation.y, rotation.z));
         if (this.wasClicked)
             this.hover(this.targetMovement);
