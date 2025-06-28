a1b495a83db01f674f562b6ab12cd0f87d58ff71
pazhersh <pazhersh@gmail.com>
2025-06-28 23:03:03 +0300

    added deck generation

--- src/TableTopPage/TableTopPage.tsx
+++ src/TableTopPage/TableTopPage.tsx
@@ -4,42 +4,50 @@ import useThree from '../useThree/useThree';
 import useOrbitControls from '../useThree/useOrbitControls';
 import useLightSource from '../useThree/useLightSource';
 import Ground from '../elements/ground';
-import Card from '../elements/card';
 import * as THREE from 'three';
 import { Element } from '../elements/types';
-
-const flipQuaternion = new THREE.Quaternion().setFromAxisAngle(new THREE.Vector3(0, 0, 1), Math.PI);
+import useCardDeck from '../elements/useCardDeck';
 
 export default function TableTopPage() {
-    const canvasRef = useRef<HTMLCanvasElement | null>(null);
+    const canvasRef = useRef<HTMLCanvasElement>(undefined);
     const [elements, setElements] = useState<Element[]>([]);
 
     const onAnimate = useCallback(() => {
         elements.forEach((element) => element.update?.());
     }, [elements]);
 
-    const { scene, world, camera } = useThree({ canvasRef, onAnimate });
-    const isLoaded = useMemo(() => (world && scene), [world, scene]);
+    const { scene, world, camera, raycaster } = useThree({ canvasRef, onAnimate });
     const controls = useOrbitControls({ camera, canvasElement: canvasRef.current });
     useLightSource({ scene });
+
+    const hoverPlaneGeometry = new THREE.PlaneGeometry(100, 100).rotateX(Math.PI / 2 * -1);
+    const hoverPlane = new THREE.Mesh(hoverPlaneGeometry);
+    hoverPlane.visible = false;
+    hoverPlane.position.setY(1);
+    hoverPlane.name = 'hoverPlane';
+    scene.add(hoverPlane);
+
+
+    const deck = useMemo(() => ({ name: 'test', cardValues: ['1', '2', '3'] }), []);
+    const { cards } = useCardDeck({
+        world,
+        scene,
+        canvasElement: canvasRef.current,
+        raycaster,
+        deck,
+        hoverAtMesh: hoverPlane
+    });
 
     useEffect(() => {
         const initElements = async () => {
             const ground = new Ground(scene, world);
-
-            const card = await Card.Create(scene, world, 'asdf');
-            card.rigidBody.setTranslation({ x: 0, y: 1, z: 0 });
-            console.log(card.rigidBody.translation());
-            card.rigidBody.setRotation(flipQuaternion);
-            // card.setLocked(true);
-
-            setElements([ground, card]);
+            setElements([ground, ...cards]);
         }
 
-        if (isLoaded) {
+        if (world && scene && cards) {
             initElements();
         }
-    }, [isLoaded, setElements])
+    }, [world, scene, cards])
 
     return <div className={styles.container} >
         <canvas ref={canvasRef} />
