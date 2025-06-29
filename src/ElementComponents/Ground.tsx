55854bc9e1abf0a317e9d9e223f89be6d0680e73
pazhersh <pazhersh@gmail.com>
2025-06-29 21:48:29 +0300

    fixed ground look

--- src/ElementComponents/Ground.tsx
+++ src/ElementComponents/Ground.tsx
@@ -8,13 +8,14 @@ const depth = 10;
 
 export default function Ground({ meshProps, rigidBodyProps }: ElementComponentProps) {
     return <mesh {...meshProps} >
-        <mesh position={[0, -depth / 2, 0]}>
-            <planeGeometry args={[width, height]} rotateX={Math.PI / 2 * -1} />
-            <meshBasicMaterial color='#0000dd' />
+        <mesh rotation={[-Math.PI / 2, 0, 0]}>
+            <planeGeometry args={[width, height]} />
+            <meshStandardMaterial color='#0000dd' />
         </mesh>
         <RigidBody type='fixed' {...rigidBodyProps}>
-            <mesh position={[0, -5, 0]}>
+            <mesh position={[0, -depth / 2, 0]}>
                 <boxGeometry args={[width, depth, height]} />
+                <meshPhongMaterial opacity={0} transparent />
             </mesh>
         </RigidBody>
     </mesh>
