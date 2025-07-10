9e199fcdabaed78054694fe20e3b854df50a6d10
pazhersh <pazhersh@gmail.com>
2025-07-10 23:41:56 +0300

    migrated tabletop to use deck store

--- src/Pages/TableTop/TableTopPage.tsx
+++ src/Pages/TableTop/TableTopPage.tsx
@@ -1,13 +1,13 @@
 import { useMemo } from 'react';
 import NavBar from '../../Components/NavBar';
-import { Deck as DeckData, loadDecks } from '../../decksUtils';
 import styles from './TableTopPage.module.css';
 
 import { Canvas } from '@react-three/fiber';
 import * as THREE from 'three';
-import TableScene from '../../Elements/TableScene';
 import Deck from '../../Elements/Deck';
+import TableScene from '../../Elements/TableScene';
 import TableTop from '../../Elements/TableTop';
+import useDecksStore from '../../useDecksStore';
 
 
 // TODO: move to using useThree to set camera
@@ -16,7 +16,8 @@ camera.position.copy(new THREE.Vector3(0.0, 4.0, -2.0));
 camera.lookAt(new THREE.Vector3(0, 0, 0));
 
 export default function TableTopPage() {
-    const decks = useMemo<DeckData[]>(() => Array.from(loadDecks().values()), []);
+    const { decks } = useDecksStore()
+    const deckValues = useMemo(() => Array.from(decks.values()), [decks])
 
     return <div className={styles.container}>
         <div className={styles.overlay}>
@@ -26,7 +27,7 @@ export default function TableTopPage() {
             <TableScene>
                 <TableTop>
 
-                    {decks.map((deck, index) =>
+                    {deckValues.map((deck, index) =>
                         <Deck key={deck.name} deck={deck} translation={[2 * index, 0, 0]} />
                     )}
 
