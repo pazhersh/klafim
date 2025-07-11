cac0e403013fd9bd4c11bc55b964a45a58c60e59
pazhersh <pazhersh@gmail.com>
2025-07-11 10:55:53 +0300

    small cleaning

--- src/Components/DeckPreview.tsx
+++ src/Components/DeckPreview.tsx
@@ -4,17 +4,22 @@ import * as THREE from 'three';
 import { Deck as DeckData } from "../useDecksStore";
 import Deck from '../Elements/Deck';
 import TableScene from '../Elements/TableScene';
+import { useMemo } from "react";
 
 type DeckPreviewProps = {
     deck: DeckData;
 }
 
-// TODO: move to using useThree to set camera
-const camera = new THREE.PerspectiveCamera(75);
-camera.position.copy(new THREE.Vector3(0.0, 2, -2.0));
-camera.lookAt(new THREE.Vector3(0, 0, 0));
 
 export default function DeckPreview({ deck }: DeckPreviewProps) {
+    const camera = useMemo(() => {
+        const camera = new THREE.PerspectiveCamera(75);
+        camera.position.copy(new THREE.Vector3(0.0, 2, -2.0));
+        camera.lookAt(new THREE.Vector3(0, 0, 0));
+
+        return camera
+    }, [])
+
     return <Canvas camera={camera}>
         <TableScene>
             <Deck deck={deck} />
