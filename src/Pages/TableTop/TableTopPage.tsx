8c79a6b4421e17d150c9902433ba03ddfdf5e0ea
pazhersh <pazhersh@gmail.com>
2025-07-11 09:47:30 +0300

    Combined TableTop and TableTopPage

--- src/Pages/TableTop/TableTopPage.tsx
+++ src/Pages/TableTop/TableTopPage.tsx
@@ -1,8 +1,18 @@
 import { useMemo } from 'react';
-import TableTop from '../../Components/TableTop';
 import styles from './TableTopPage.module.css';
 import NavBar from '../../Components/NavBar';
 import useDecksStore from '../../useDecksStore';
+
+import { Canvas } from '@react-three/fiber';
+import * as THREE from 'three';
+
+import Deck from '../../Elements/Deck';
+import TableScene from '../../Elements/TableScene';
+
+// TODO: move to using useThree to set camera
+const camera = new THREE.PerspectiveCamera(75);
+camera.position.copy(new THREE.Vector3(0.0, 4.0, -2.0));
+camera.lookAt(new THREE.Vector3(0, 0, 0));
 
 export default function TableTopPage() {
     const { decks } = useDecksStore()
@@ -12,6 +22,12 @@ export default function TableTopPage() {
         <div className={styles.overlay}>
             <NavBar />
         </div>
-        <TableTop className={styles.canvas} decks={deckValues} />
+        <Canvas className={` ${styles.canvas}`} camera={camera} >
+            <TableScene>
+                {deckValues.map((deck, index) =>
+                    <Deck key={deck.name} deck={deck} translation={[2 * index, 0, 0]} />
+                )}
+            </TableScene>
+        </Canvas>
     </div>;
 }
