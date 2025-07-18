52c117e70182455c94bbcdd08f2d36809b715a92
pazhersh <pazhersh@gmail.com>
2025-07-18 23:23:45 +0300

    improve navbar

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -1,9 +1,28 @@
 import { NavLink } from 'react-router';
 import styles from './NavBar.module.css';
 
+type Link = {
+    to: string;
+    label: string;
+}
+
+const links: Link[] = [
+    { to: '/manage-decks', label: 'Edit Decks' },
+    { to: '/table-top', label: 'Table-Top' },
+];
+
+function Seperator() {
+    return <div className={styles.seperator} />
+}
+
 export default function NavBar() {
     return <div className={styles.container}>
-        <NavLink className={styles.link} to='/manage-decks'>Edit Decks</NavLink>
-        <NavLink className={styles.link} to='/table-top'>Table-Top</NavLink>
+        {links.slice(0, length - 1).map(link =>
+            <>
+                <NavLink key={link.to} className={styles.link} to={link.to}>{link.label}</NavLink>
+                <Seperator />
+            </>
+        )}
+        <NavLink className={styles.link} to={links.at(-1)!.to}>{links.at(-1)!.label}</NavLink>
     </div>
 }
