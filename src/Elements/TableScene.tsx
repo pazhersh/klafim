18c9aed9618ed9cae813af8bbb39bf1da2ebce2c
pazhersh <pazhersh@gmail.com>
2025-07-19 17:22:05 +0300

    added background

--- src/Elements/TableScene.tsx
+++ src/Elements/TableScene.tsx
@@ -1,16 +1,27 @@
 import { Physics } from '@react-three/rapier';
-import { Suspense } from 'react';
+import { Suspense, useEffect } from 'react';
 
+import { useLoader, useThree } from '@react-three/fiber';
+import { EquirectangularReflectionMapping, TextureLoader } from 'three';
+import OrbitControls from '../Elements/OrbitControls';
 import Table from '../Elements/Table';
 import HoldContext from './HoldContext';
 import HoldPlane from './HoldPlane';
-import OrbitControls from '../Elements/OrbitControls';
 
 type TableSceneProps = {
     children: React.ReactNode;
 }
 
 export default function TableScene({ children }: TableSceneProps) {
+    const skyBox = useLoader(TextureLoader, '/public/PANO_20250719_170347.jpg');
+    const { scene } = useThree();
+
+    useEffect(() => {
+        // TODO: rotate texturew so by default you face to clock
+        skyBox.mapping = EquirectangularReflectionMapping;
+        scene.background = skyBox;
+    }, [skyBox, scene]);
+
     return <>
         <ambientLight intensity={Math.PI / 2} />
         <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
