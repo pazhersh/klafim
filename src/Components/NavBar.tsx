a5f622233adeb36de359f51193451c43e26781b9
pazhersh <pazhersh@gmail.com>
2025-08-29 15:38:00 +0300

    small fix

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -1,5 +1,6 @@
 import { NavLink } from 'react-router';
 import styles from './NavBar.module.css';
+import React from 'react';
 
 type Link = {
     to: string;
@@ -19,10 +20,10 @@ function Seperator() {
 export default function NavBar() {
     return <div className={styles.container}>
         {links.slice(0, length - 1).map(link =>
-            <>
+            <React.Fragment key={link.to}>
                 <NavLink key={link.to} className={styles.link} to={link.to}>{link.label}</NavLink>
                 <Seperator />
-            </>
+            </React.Fragment>
         )}
         <NavLink className={styles.link} to={links.at(-1)!.to}>{links.at(-1)!.label}</NavLink>
     </div>
